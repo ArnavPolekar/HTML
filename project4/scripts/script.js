72169ee252061057c83fafa79ca6f1a8cb64a4db
Sanket Polekar <arnavsp26@gmail.com>
2022-03-23 22:39:56 +0530

    pop up js

---
+++ project4/scripts/script.js
@@ -0,0 +1,7 @@
+function showPop(popstate) {
+    if(popstate == 'on') {
+    document.getElementById("optionsPop").style.display = "block";
+    }else if(popstate == 'off') {
+        document.getElementById("optionsPop").style.display = "none";        
+    }
+}
