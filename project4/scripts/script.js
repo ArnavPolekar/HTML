c5c0aec6e5cd8e50435bc74bec3148093c100990
Sanket Polekar <arnavsp26@gmail.com>
2022-03-24 21:24:28 +0530

    pop edit

--- project4/scripts/script.js
+++ project4/scripts/script.js
@@ -1,7 +1,10 @@
 function showPop(popstate) {
-    if(popstate == 'on') {
-    document.getElementById("optionsPop").style.display = "block";
-    }else if(popstate == 'off') {
-        document.getElementById("optionsPop").style.display = "none";        
+
+    if (popstate == 'on') {
+        document.getElementById("optionsPop").style.display = "block";
+    } else if (popstate == 'off') {
+        document.getElementById("optionsPop").style.display = "none";
     }
 }
+
+
